aaeace29ca2401a6d6421aa587eebaf08946929a
Abaijan <fghf09647@gmail.com>
2024-10-16 22:36:48 +0600

    date picker is added

--- app/src/main/java/com/example/to_dolistapplication/AddNoteActivity.java
+++ app/src/main/java/com/example/to_dolistapplication/AddNoteActivity.java
@@ -1,21 +1,68 @@
 package com.example.to_dolistapplication;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import java.util.Calendar;
+
 
 public class AddNoteActivity extends AppCompatActivity {
-
-    TextView heaerText;
+    ImageButton imageButton;
+    ImageButton selectDateBtn;
+    TextView selectDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
-        heaerText = findViewById(R.id.textTitle);
-        heaerText.setText("Главная");
+
+
+
+        // back to tha main
+        imageButton = findViewById(R.id.back_btn);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AddNoteActivity.this, MainActivity.class);
+                        startActivity(intent);
+            }
+        });
+        // back to the main
+
+        //select date
+        selectDate = findViewById(R.id.selectedDate);
+        selectDateBtn = findViewById(R.id.selectBtn);
+
+        selectDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        AddNoteActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @SuppressLint("SetTextI18n")
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                selectDate.setText(year + "-" +  (monthOfYear + 1 ) + "-" + dayOfMonth);
+                            }
+                        },
+                        year,month,day);
+                datePickerDialog.show();
+            }
+        });
     }
 
 }
